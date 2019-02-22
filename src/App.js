1090b64b41f8734fe9cf9b037ea6c09770b49500
Renuka <renukaalurkar@gmail.com>
2019-02-22 11:26:24 -0600

    react-to-do-state-props'

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  //the constructor below is used to assign component's initial state
+  //when we call our constructor, we must call the parent constructor, and hence the super.
+  constructor(props){
+    super(props);
+    //we are initializing the state of our component with todos array. This array has keys and properties.
+    this.state={
+      todos:[{description:'walk the cat', isCompleted:true},
+             {description:'throw the dishes away', isCompleted:false},
+             {description:'Buy new dishes', isCompleted:false}
+
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
       <ul>
-      <ToDo/>
-      <ToDo/>
+      //Now, we have to render or show the initial state of the component with the above todo list
+      { this.state.todos.map( (todo, index) =>
+            
+            <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted}/>
+          )}
       </ul>
-      
+
 
 
 
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,11 @@ import React, {Component} from 'react';
 class ToDo extends Component{
  render(){
   return(
-   <li>A todo will go here </li>
+    //the description here we are getting from the App component
+   <li>
+   <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+    </li>
   );
  }
 }
