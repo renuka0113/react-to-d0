3ac96dd275085ef6635aa947cfbd24c7f1c3d3fc
Renuka <renukaalurkar@gmail.com>
2019-02-28 12:22:36 -0600

    delete button functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   //when we call our constructor, we must call the parent constructor, and hence the super.
   constructor(props){
     super(props);
-    //we are initializing the state of our component with todos array. This array has keys and properties.
+    //we are initializing the state of our component with todos array. This array has keys and properties
     this.state={
       todos:[{description:'walk the cat', isCompleted:true},
              {description:'throw the dishes away', isCompleted:false},
@@ -31,18 +31,26 @@ class App extends Component {
  }
 
   toggleComplete(index) {
-
+{/*In the line below, we are making a copy of the array this.state.todos
+   We could have just done const todos=this.state.todos, but that will change the value of the actual
+   array this.state.todos and we dont want that to happen, hence we use splice() here.
+ */}
     const todos = this.state.todos.slice();
+    {/*Here we are creating a variable todo which is a singl element of the array todos.
+      So, todos[index] is a single element of the new array todos
+    */}
     const todo = todos[index];
     todo.isCompleted = todo.isCompleted ? false : true;
+    
     this.setState({ todos: todos });
 
   }
 
   deleteTodo(index){
-  console.log(this.state);
+
   const remainingTodos =this.state.todos.filter(todo => (this.state.todos.indexOf(todo) !== index));
   this.setState({ todos: remainingTodos});
+
 
   }
 
@@ -52,10 +60,14 @@ class App extends Component {
     return (
       <div className="App">
       <ul>
-      //Now we have to render or show the initial state of the component with the above todo list
+      {/*Now we have to render or show the initial state of the component with the above todo
+         list*/}
       { this.state.todos.map( (todo, index) =>
-
-        <ToDo key={index} deleteTodo={this.deleteTodo} description={todo.description} isCompleted={todo.isCompleted} toggleComplete={ () => this.toggleComplete(index)}/>)}
+{/* In the line below, we are passing key index, functions as properties
+to the ToDo component. See how the beginning of the line has ToDo, the name of the component.
+This is the component to which we are passing all these functions and variables
+*/}
+        <ToDo key={index} deleteTodo={ () => this.deleteTodo(index)} description={todo.description} isCompleted={todo.isCompleted} toggleComplete={ () => this.toggleComplete(index)}/>)}
       </ul>
        <input type="text" value={ this.state.newTodoDescription }onChange={ (e) => this.handleChange(e) }/>
           <input onClick={ (e) => this.handleSubmit(e) } type="button" name="submit" value="Submit"/>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 
 class ToDo extends Component{
+
  render(){
   return(
-    //the description here we are getting from the App component
+
 
    <li>
+{/* this.props.isCompleted, toffleComplete, description,deleteTodo are all defined in the React component
+  and are passed here to the ToDo component
+*/}
    <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete }/>
          <span>{ this.props.description }</span>
-   <input onClick={ this.props.key } type="button" name="delete" value="Delete"/>
+         {/*here we create the Delete button and create the onClick event handler.
+           To the onClick event handler we pass the deleteTodo function.The deleteTodo function is
+           defined in the App.js. This means that we are saying,when the Delete button is "clicked",the deleteTodo method should
+           be called
+         */}
+   <input onClick={ this.props.deleteTodo} type="button" name="delete" value="Delete"/>
    </li>
 
 
